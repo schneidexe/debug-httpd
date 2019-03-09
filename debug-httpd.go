657e11f517c67c77c2a0400286e57bb9bce28078
schneidexe <git@schneidexe.de>
2019-03-09 16:59:10 +0100

    add query string

--- debug-httpd.go
+++ debug-httpd.go
@@ -8,9 +8,10 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Context-Path: /%s\n", r.URL.Path[1:])
-    fmt.Fprintf(w, "Host: %s\n", r.Host)
     fmt.Fprintf(w, "Remote-Addr: %s\n", r.RemoteAddr)
+    fmt.Fprintf(w, "Host: %s\n", r.Host)
+    fmt.Fprintf(w, "Path: %s\n", r.URL.Path)
+    fmt.Fprintf(w, "Query: %s\n", r.URL.RawQuery)
     fmt.Fprintf(w, "Headers:\n")
     for k,vs := range r.Header {
         fmt.Fprintf(w, "\t%s:\n", k)
